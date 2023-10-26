ad68e54e1544fbb40e10a072a8d0ccaab9db4d6a
HeeJae RA <54802957+HeeJaeRA@users.noreply.github.com>
2023-10-26 16:42:43 +0900

    .

--- helloWeb/src/main/webapp/js/string1.js
+++ helloWeb/src/main/webapp/js/string1.js
@@ -10,3 +10,9 @@ console.log(str1.toUpperCase());
 
 let result = " 공백 제거 합니다.         ".trim();
 console.log(result, '문자열 길이: ', result.length);
+
+result = "Hello, World, Nice, World".replace(',', '.');
+console.log(result);
+
+result = "Hello, World, Nice, World".replace(/,/g, '.');
+console.log(result);
---
+++ helloWeb/src/main/webapp/js/string2.js
@@ -0,0 +1,37 @@
+
+// 공백 기준으로 단어 크기 5이상인 단어 출력
+const words = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, nesciunt. Ex, unde doloremque minus voluptate natus laborum explicabo ut veritatis, ipsa quos et corporis id, aliquid nesciunt consequuntur dolores deleniti!";
+let word;
+let str5size = '';
+word = words.split(' ');
+for (let i = 0; i < word.length; i++) {
+    if (word[i].length > 5) {
+        // console.log(word[i]);
+        str5size += word[i] + " ";
+    }
+}
+console.log('크기 6이상: ', str5size);
+
+// 생년월일 입력 > 남자 여자
+function checkGender(ssn) {
+    //950305 3678532
+    let strGen = ssn.replace(/\s/g, '');
+
+    if (strGen.charAt(6) == '1' || strGen.charAt(6) == '3') {
+        return '남자';
+    } else if (strGen.charAt(6) == '2' || strGen.charAt(6) == '4') {
+        return '여자';
+    }
+}
+
+console.log('성별: ', checkGender('950305 3678532'));
+
+// 파일명과 확장자
+let file = "d:/temp/sample/book.xls";
+let fileName, fileExt;
+
+fileExt = file.split('.', -1).reverse()[0];
+fileName = file.split('.', -1).reverse()[1].split('/', -1).reverse()[0];
+
+console.log('파일이름: ', fileName);
+console.log('확장자: ', fileExt);
