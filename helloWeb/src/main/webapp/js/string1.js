894501e9d01bb9cbd42298c59787f8495454e94b
HeeJae RA <54802957+HeeJaeRA@users.noreply.github.com>
2023-10-26 15:12:46 +0900

    .

---
+++ helloWeb/src/main/webapp/js/string1.js
@@ -0,0 +1,12 @@
+let str1 = "Hello";
+let str2 = new String("Hello");
+
+console.log(typeof str1, typeof str2);
+
+console.log(str1 == str2);
+console.log(str1 === str2);
+
+console.log(str1.toUpperCase());
+
+let result = " 공백 제거 합니다.         ".trim();
+console.log(result, '문자열 길이: ', result.length);
