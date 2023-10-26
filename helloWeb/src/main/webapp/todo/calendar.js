63baee35115681e05ec4e6c9c5842fc9257c7b6c
HeeJae RA <54802957+HeeJaeRA@users.noreply.github.com>
2023-10-26 09:06:17 +0900

    .

--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -3,52 +3,52 @@ const yo = ['일요일', '월요일', '화요일', '수요일', '목요일', '�
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const calendar = {
-    html: '',
+	html: '',
 
-    makeHead() {
-        let strHead = "";
+	makeHead() {
+		let strHead = "";
 
-        strHead += `<table border ="1"><thead><tr>`;
+		strHead += `<table border ="1"><thead><tr>`;
 
-        for (let yoil in yo) {
-            strHead += '<th>' + yo[yoil] + '</th>';
-        }
+		for (let yoil in yo) {
+			strHead += '<th>' + yo[yoil] + '</th>';
+		}
 
-        strHead += '</tr></thead>';
+		strHead += '</tr></thead>';
 
-        this.html += strHead;
-    },
+		this.html += strHead;
+	},
 
-    makeBody(mon) {
-        let obj = this;
-        let strBody = "";
-        strBody += '<tbody><tr>'
+	makeBody(mon) {
+		let obj = this;
+		let strBody = "";
+		strBody += '<tbody><tr>'
 
-        for (let i = 1; i <= months[mon]; i++) {
-            strBody += `<td>${i}</td>`
-            if (i % 7 == 0) {
-                strBody += '<tr></tr>'
-            }
-        }
-        strBody += '</tr></tbody></table><br>'
+		for (let i = 1; i <= months[mon]; i++) {
+			strBody += `<td>${i}</td>`
+			if (i % 7 == 0) {
+				strBody += '<tr></tr>'
+			}
+		}
+		strBody += '</tr></tbody></table><br>'
 
-        obj.html += strBody;
-    },
+		obj.html += strBody;
+	},
 
-    makeCalendar(dom) {
-        dom.innerHTML = this.html;
-    }
+	makeCalendar(dom) {
+		dom.innerHTML = this.html;
+	}
 }
 
-// calendar.makeHead();
+calendar.makeHead();
 
-// calendar.makeBody(9);
+calendar.makeBody(9);
 
-// calendar.makeCalendar(document.getElementById('show'));
-
-for (let i = 0; i < 12; i++) {
-    calendar.makeHead();
-
-    calendar.makeBody(i);
-}
 calendar.makeCalendar(document.getElementById('show'));
+
+// for (let i = 0; i < 12; i++) {
+//    calendar.makeHead();
+
+//    calendar.makeBody(i);
+// }
+// calendar.makeCalendar(document.getElementById('show'));
