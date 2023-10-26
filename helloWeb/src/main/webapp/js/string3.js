1cae05fee704f090af3f20859b94149f1193d7d0
HeeJae RA <rhj7369@nate.com>
2023-10-26 20:49:48 +0900

    .

--- helloWeb/src/main/webapp/js/string3.js
+++ helloWeb/src/main/webapp/js/string3.js
@@ -1,17 +1,16 @@
 let today = new Date();
 
-today.setFullYear(2022);
+today.setFullYear(2023);
 today.setMonth(0);
 today.setDate(1);
-today.setHours(10);
+today.setHours(1);
 
 console.log(today.toString());
 
 function dateFormat(today) {
-    let mon = (today.getMonth() + 1).toString();
-    console.log(mon);
 
-    return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    return (today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + today.getDate()).slice(-2) + " " + ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2) + ":" + ("0" + today.getSeconds()).slice(-2));
+
 }
 
 console.log(dateFormat(today));
