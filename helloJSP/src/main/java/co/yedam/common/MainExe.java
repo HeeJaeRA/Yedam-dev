a829638f7307f2119d36cd5c385aa38d4416e92f
HeeJae RA <54802957+HeeJaeRA@users.noreply.github.com>
2023-11-03 16:01:30 +0900

    Board

--- helloJSP/src/main/java/co/yedam/common/MainExe.java
+++ helloJSP/src/main/java/co/yedam/common/MainExe.java
@@ -30,5 +30,32 @@ public class MainExe {
 		
 		System.out.println(dao.list());
 	}
+	
+	// DB
+//	CREATE TABLE BOARD (
+//		    BOARD_NO        NUMBER,
+//		    TITLE           VARCHAR2(100)   NOT NULL,
+//		    CONTENT         VARCHAR2(1000)  NOT NULL,
+//		    WRITER          VARCHAR2(30)    NOT NULL,
+//		    WRITE_DATE      DATE            DEFAULT SYSDATE,
+//		    VIEW_CNT        NUMBER          DEFAULT 0,
+//		    IMAGE           VARCHAR2(100),
+//		    LAST_UPDATE     DATE            DEFAULT SYSDATE
+//		);
+//
+//		ALTER TABLE BOARD ADD CONSTRAINT BOARD_PK PRIMARY KEY (BOARD_NO);
+//
+//		CREATE SEQUENCE SEQ_BOARD;
+//
+//		SELECT * FROM BOARD;
+//
+//		COMMIT;
+//
+//		INSERT INTO BOARD (BOARD_NO, TITLE, CONTENT, WRITER)
+//		VALUES (SEQ_BOARD.NEXTVAL, '첫번째제목', '첫번째내용', 'USER01');
+//		INSERT INTO BOARD (BOARD_NO, TITLE, CONTENT, WRITER)
+//		VALUES (SEQ_BOARD.NEXTVAL, '두번째제목', '두번째내용', 'USER02');
+//		INSERT INTO BOARD (BOARD_NO, TITLE, CONTENT, WRITER)
+//		VALUES (SEQ_BOARD.NEXTVAL, '세번째제목', '세번째내용', 'USER01');
 
 }
