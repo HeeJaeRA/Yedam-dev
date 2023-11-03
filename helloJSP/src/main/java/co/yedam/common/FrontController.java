a563db6965cab818d1dffb33b4daaec5059fe56e
HeeJae RA <54802957+HeeJaeRA@users.noreply.github.com>
2023-11-03 16:00:04 +0900

    Board

--- helloJSP/src/main/java/co/yedam/common/FrontController.java
+++ helloJSP/src/main/java/co/yedam/common/FrontController.java
@@ -9,29 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import co.yedam.board.web.BoardListControl;
+
 public class FrontController extends HttpServlet {
 
 	Map<String, Command> map = new HashMap<>();
-	
+
 	@Override
 	public void init() throws ServletException {
-//		map.put("/FirstServlet.do", new FirstControl());
-//		map.put("/second.do", new SecondControl());
+		map.put("/boardList.do", new BoardListControl());
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html; charset=utf-8");
-//		System.out.println("FrontController");
-		
+
 		String uri = req.getRequestURI();
 		String context = req.getServletContext().getContextPath();
 		String page = uri.substring(context.length());
-		
-//		System.out.println(page);
-		
+
 		Command controller = map.get(page);
 		controller.execute(req, resp);
 	}
-	
+
 }
